cb6c0412250ca14f471871266784a4e669fb3d97
Surya152002 <108388181+Surya152002@users.noreply.github.com>
2022-07-05 00:28:18 +0530

    Add files via upload

---
+++ Battery notifier.py
@@ -0,0 +1,13 @@
+from pynotifier import Notification
+import psutil
+battery = psutil.sensors_battery()
+percent = battery.percent
+
+def check():
+    if percent == 99 or percent == 100 :
+        Notification("Battery Percentage",str(percent) +"%Percentage Remaining"+ "Un-Plug the Charger",duration=100000).send()
+
+    if percent == 15:
+        Notification("Battery Percentage", str(percent) + "%Percentage Remaining" + "Plug In Charger",duration=1000000).send()
+        check()
+Notification("Battery Percentage",str(percent) +"%Percentage Remaining"+ "Un-Plug the Charger",duration=100000).send()
